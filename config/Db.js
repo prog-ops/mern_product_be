7fc37102c7f6a7db9deaf5eb8fff98778ea4a73b
prog <email@gmail.com>
2023-05-16 13:17:10 +0700

    Modify config

--- config/Db.js
+++ config/Db.js
@@ -1,11 +1,11 @@
 import {Sequelize} from "sequelize";
 
 export const db = new Sequelize(
-    'produkdb',
-    'root',
-    '',
+    process.env.DB_DATABASE,//'produkdb',
+    process.env.DB_USER,//'root',
+    process.env.DB_PASSWORD,//'',
     {
-      host: 'localhost',
+      host: process.env.DB_HOST,//'localhost',
       dialect: 'mysql'
     }
 )
